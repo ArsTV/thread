978d7a41fece94f5264e9584a210508dace8c73e
DELL <DELL@DELL-Laptop-M5A6LK>
2017-04-18 18:40:32 -0400

    Tab chenged for visual reading

--- ThreadTest/src/Test2/Main.java
+++ ThreadTest/src/Test2/Main.java
@@ -4,8 +4,8 @@ public class Main {
 	public static void main(String[] args) {		
 		Store store = new Store();
 		Producer producer = new Producer(store);
-        Customer consumer = new Customer(store);
-        new Thread(producer).start();
-        new Thread(consumer).start();
+		Customer consumer = new Customer(store);
+		new Thread(producer).start();
+		new Thread(consumer).start();
     }
 }
